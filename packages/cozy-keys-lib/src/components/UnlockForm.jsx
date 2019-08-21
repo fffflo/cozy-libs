dffc2ded13dad3934dbd021efd2c1d80da88c521
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-21 15:41:33 +0200

    feat: Unlock vault

--- packages/cozy-keys-lib/src/components/UnlockForm.jsx
+++ packages/cozy-keys-lib/src/components/UnlockForm.jsx
@@ -10,12 +10,18 @@ import Field from 'cozy-ui/transpiled/react/Field'
 import Button from 'cozy-ui/transpiled/react/Button'
 import CloudIcon from '../../assets/icon-cozy-security.svg'
 import palette from 'cozy-ui/transpiled/react/palette'
-import {translate} from 'cozy-ui/transpiled/react/I18n'
-// const { client } = React.useContext(VaultContext)
+import { translate } from 'cozy-ui/transpiled/react/I18n'
+
+import { withVaultClient } from './VaultContext'
 
 class UnlockForm extends React.Component {
   state = {
     password: ''
+  }
+
+  unlockVault() {
+    const { vaultClient } = this.props
+    vaultClient.unlock()
   }
 
   render() {
@@ -45,7 +51,11 @@ class UnlockForm extends React.Component {
             fullwidth
             className="u-w-100 u-white"
             secondaryComponent={({ visible }) =>
-              visible ? <Icon aria-label={t('unlock.show')} icon="eye-closed" /> : <Icon icon="eye" aria-label={t('unlock.hide')} />
+              visible ? (
+                <Icon aria-label={t('unlock.show')} icon="eye-closed" />
+              ) : (
+                <Icon icon="eye" aria-label={t('unlock.hide')} />
+              )
             }
             labelProps={{ className: 'u-white' }}
           />
@@ -56,6 +66,7 @@ class UnlockForm extends React.Component {
             label={t('unlock.unlock')}
             theme="secondary"
             className="u-w-100-t u-dodgerBlue"
+            onClick={this.unlockVault.bind(this)}
           />
         </ModalFooter>
       </Modal>
@@ -63,4 +74,4 @@ class UnlockForm extends React.Component {
   }
 }
 
-export default translate()(UnlockForm)
+export default withVaultClient(translate()(UnlockForm))
--- packages/cozy-keys-lib/src/components/VaultContext.jsx
+++ packages/cozy-keys-lib/src/components/VaultContext.jsx
@@ -10,7 +10,10 @@ class VaultProvider extends React.Component {
 
   componentDidMount() {
     const client = new VaultClient(this.props.url)
-    client.onUnlock = () => this.forceUpdate()
+    client.onUnlock = () => {
+      console.log('force update')
+      this.forceUpdate()
+    }
     this.setState({
       client
     })
@@ -26,4 +29,17 @@ class VaultProvider extends React.Component {
   }
 }
 
-export { VaultContext, VaultProvider }
+const withVaultClient = BaseComponent => {
+  const Component = props => (
+    <VaultContext.Consumer>
+      {({ client }) => <BaseComponent vaultClient={client} {...props} />}
+    </VaultContext.Consumer>
+  )
+
+  Component.displayName = `withVaultClient(${BaseComponent.displayName ||
+    BaseComponent.name})`
+
+  return Component
+}
+
+export { VaultContext, VaultProvider, withVaultClient }
