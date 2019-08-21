2b64f04a7fa0018cf3d37615f0475d884ae440c1
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-21 15:23:23 +0200

    feat: Integrate cozy client

--- packages/cozy-keys-lib/src/components/VaultContext.jsx
+++ packages/cozy-keys-lib/src/components/VaultContext.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import VaultClient from '../VaultClient'
 
-const client = new VaultClient()
 const VaultContext = React.createContext()
 
 class VaultProvider extends React.Component {
+  state = {
+    client: null
+  }
+
   componentDidMount() {
+    const client = new VaultClient(this.props.url)
     client.onUnlock = () => this.forceUpdate()
+    this.setState({
+      client
+    })
   }
 
   render() {
-    return (
+    const { client } = this.state
+    return client ? (
       <VaultContext.Provider value={{ client }}>
         {this.props.children}
       </VaultContext.Provider>
-    )
+    ) : null
   }
 }
 
--- packages/playgrounds/src/cozy-keys.jsx
+++ packages/playgrounds/src/cozy-keys.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Route } from 'react-router'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import App from './common/App'
+import client from './common/client'
 import 'cozy-ui/transpiled/react/stylesheet.css'
 import { Sprite } from 'cozy-ui/transpiled/react/Icon'
 import 'cozy-ui/dist/cozy-ui.min.css'
+import { withClient } from 'cozy-client'
+
 import { VaultProvider, VaultUnlocker } from '../../cozy-keys-lib/dist'
 
+const reducer = combineReducers({
+  cozy: client.reducer()
+})
 
-const App = () => (
-  <VaultProvider>
+const store = createStore(reducer)
+
+const Vault = ({ client }) => (
+  <VaultProvider url={client.getStackClient().uri}>
     <Sprite />
     <VaultUnlocker>
       <div>Unlocked</div>
@@ -15,7 +27,11 @@ const App = () => (
   </VaultProvider>
 )
 
+const VaultWithClient = withClient(Vault)
+
 ReactDOM.render(
-    <App />,
+  <App client={client} existingStore={store}>
+    <Route path="/" component={VaultWithClient} />
+  </App>,
   document.querySelector('#app')
 )
